f5fcbf535162c04c88950f5acedb8f0e4957b8ad
dmrysev <dmrysev@gmail.com>
2020-01-24 13:46:29 +0200

    Link shared library

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "my_static_lib/addition.hpp"
+#include "my_dynamic_lib/multiplication.hpp"
 
 using namespace MyProject;
 
 int main() {
     double result = MyStaticLib::Addition::add2(2);
     std::cout << "2 + 2 = " << result << std::endl;
+
+    double result2 = MyDynamicLib::Multiplication::multBy2(3);
+    std::cout << "3 * 2 = " << result2 << std::endl;
+
     return 0;
 }
